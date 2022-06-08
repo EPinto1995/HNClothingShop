905f1ae59515e9454d5d19f4e57fa97cedc4c539
EPinto1995 <EPinto@DESKTOP-B7A6G2I>
2022-06-07 20:48:14 -0700

    Agregando Entigy Framework Code First

--- HNClothingShop/HNClothingShop.BL/ProductosBL.cs
+++ HNClothingShop/HNClothingShop.BL/ProductosBL.cs
@@ -8,38 +8,18 @@ namespace HNClothingShop.BL
 {
     public class ProductosBL
     {
+        Contexto _contexto;
+        public List<Producto> ListadeProductos { get; set; }
+        public ProductosBL()
+        {
+            _contexto = new Contexto();
+            ListadeProductos = new List<Producto>();
+        }
+
         public List<Producto> ObtenerProductos()
         {
-            var producto1 = new Producto();
-            producto1.Id = 1;
-            producto1.Descripcion = "Falda";
-            producto1.Precio =  50;
-
-            var producto2 = new Producto();
-            producto2.Id = 2;
-            producto2.Descripcion = "Vestido";
-            producto2.Precio = 150;
-
-
-            var producto3 = new Producto();
-            producto3.Id = 3;
-            producto3.Descripcion = "Jean";
-            producto3.Precio = 450;
-
-
-            var producto4 = new Producto();
-            producto4.Id = 4;
-            producto4.Descripcion = "Camiseta Polo";
-            producto4.Precio = 180;
-
-
-            var listadeProductos = new List<Producto>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
-            listadeProductos.Add(producto4);
-
-            return listadeProductos;
+            ListadeProductos = _contexto.Productos.ToList();
+            return ListadeProductos;
         }
     }
 }
